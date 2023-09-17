b7fe4863b68f69ce958ab8825ce7c8a93f25b520
M4k4v3ll1 <marioman2005@mail.ru>
2023-09-17 13:30:19 +0400

    project saving for laptop

--- tii-app/src/index.js
+++ tii-app/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
-import state from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {addPost} from "./redux/state"
-import {updateNewPostText} from "./redux/state"
-import {subscribe} from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -15,15 +12,15 @@ let rerenderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+                <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>
     );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
